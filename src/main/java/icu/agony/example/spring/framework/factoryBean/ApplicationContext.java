fe0e83d340e664e8474d7b797bee3c1dcf9b6406
LiuYun <lymaker@126.com>
2024-01-28 18:06:48 +0800

    fix

--- src/main/java/icu/agony/example/spring/framework/factoryBean/ApplicationContext.java
+++ src/main/java/icu/agony/example/spring/framework/factoryBean/ApplicationContext.java
@@ -3,8 +3,9 @@ package icu.agony.example.spring.framework.factoryBean;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApplicationContext {
+
     public static void main(String[] args) {
-        final String packageName = "icu.agony.example.spring.factoryBean";
+        final String packageName = ApplicationContext.class.getPackageName();
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(packageName);
         // 第一次获取
         IdCount idCount1 = applicationContext.getBean(IdCount.class);
@@ -13,7 +14,10 @@ public class ApplicationContext {
         IdCount idCount2 = applicationContext.getBean(IdCount.class);
         System.out.println(idCount2); // out: 2
         // 获取生产bean的bean
+        // 注意：直接获取名称为 idCountFactoryBean 的 bean 是获取不到的的，需要添加前缀 & ，因为它是一个 FactoryBean
+        // see also: org.springframework.beans.factory.BeanFactory.FACTORY_BEAN_PREFIX
         IdCountFactoryBean idCountFactoryBean = (IdCountFactoryBean) applicationContext.getBean("&idCountFactoryBean");
         System.out.printf("idCount: %s%n",idCountFactoryBean.getIdCount());
     }
+
 }
